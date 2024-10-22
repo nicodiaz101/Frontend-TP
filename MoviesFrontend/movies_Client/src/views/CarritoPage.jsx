aee1232f5bf35076c9449eac9730804a93bff61d
facuZ <facundo89je@gmail.com>
2024-10-21 22:26:20 -0300

    Carrito implementado

---
+++ MoviesFrontend/movies_Client/src/views/CarritoPage.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import Carrito from '../components/Carrito/Carrito'
+import Footer from '../components/Footer'
+import './CarritoPage.css'
+
+const CarritoPage = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+            <div className="cart-container">
+                <Carrito></Carrito>
+            </div>
+        </>
+    )
+}
+
+export default CarritoPage;
